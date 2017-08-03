4390cba38f304fc8301f6bae3aeb32cf9f31bb6a
iamtanbir <tanu.tanbirul@gmail.com>
2017-08-03 19:52:49 +0600

    hi rahadi

--- Blood_Donate_Management_System/Blood_Donate_Management_System/Program.cs
+++ Blood_Donate_Management_System/Blood_Donate_Management_System/Program.cs
@@ -17,7 +17,7 @@ namespace Blood_Donate_Management_System
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-           //ffdstgdsg
+            Console.WriteLine("Hi rahadi");
         }
     }
 }
